32c034b7eb720fde48fdf5c693e400777f5b6c62
Joao <joaoviniciusvitral@hotmail.com>
2022-07-06 22:38:24 -0300

    first commit
    Co-authored-by: Marcelo Bracet <MarceloBxD@users.noreply.github.com>

---
+++ src/App/index.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Header } from '../section'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
